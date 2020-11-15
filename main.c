f50becc10338ad482521dd88eab6d314efb23520
matumoto <matumotosizuoka@gmail.com>
2020-11-15 14:49:55 +0900

    割り算を作ってあげました。^^

--- main.c
+++ main.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 
 int main(){
-    int a,b;
-    char c;
+	int a,b;
+	char c;
 
-    scanf("%d %c %d",&a,&c,&b);
+	scanf("%d %c %d",&a,&c,&b);
 
-    if(c == '+') {
-        printf("%d\n",a +b );
-    }
-    if(c == '-'){
-        printf("%d\n",a - b);
-    }
+	if(c == '+') {
+		printf("%d\n",a + b);
+	}
+	if(c == '-'){
+		printf("%d\n",a - b);
+	}
+	if(c == '/'){
+		if(b == 0){
+			printf("0で割り算をしようとしていませんか？\n");
+			return 0;
+		}
+		printf("%d\n",a / b);
+	}
 
-    return 0;
+	return 0;
 }

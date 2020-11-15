b7b241f6406d2be4c5882d87151e2e64b2694a90
matumoto-pandd <s1280136@u-aizu.ac.jp>
2020-11-15 15:17:45 +0900

    無限ループひゃっぽう

--- main.c
+++ main.c
@@ -4,23 +4,26 @@ int main(){
     int a,b;
     char c;
 
-    scanf("%d %c %d",&a,&c,&b);
+    while(1){
 
-    if(c == '+') {
-        printf("%d\n",a +b );
-    }
-    if(c == '-'){
-        printf("%d\n",a - b);
-    }
-    if(c == '*'){
-        printf("%d\n",a * b);
-    }
-    if(c == '/'){
-        if(b == 0){
-	    printf("0で割り算をしようとしていませんか？\n");
-	    return 0;
-	}
-	printf("%d\n",a / b);
+        scanf("%d %c %d",&a,&c,&b);
+    
+        if(c == '+') {
+            printf("%d\n",a +b );
+        }
+        if(c == '-'){
+            printf("%d\n",a - b);
+        }
+        if(c == '*'){
+            printf("%d\n",a * b);
+        }
+        if(c == '/'){
+            if(b == 0){
+    	        printf("0で割り算をしようとしていませんか？\n");
+    	        return 0;
+    	    }
+    	    printf("%d\n",a / b);
+        }
     }
     return 0;
 }
